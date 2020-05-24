d7e455a304232ea8f891a06a054f2b5f8b4b6b54
Shreyash Mishra <56593418+Shreyashm16@users.noreply.github.com>
2020-05-24 23:10:00 +0530

    Create LADDU.cpp

---
+++ DSA Learning Series/Contest 1/LADDU.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+int main() {
+    ll t;
+    cin >> t;
+    while(t--){
+        ll act,n=400,tot=0;
+        string nat;
+        cin >> act >> nat;
+        if(nat=="INDIAN")
+            n=200;
+        while(act--){
+            string s;
+            cin >> s;
+            if(s=="CONTEST_WON"){
+                ll r;
+                cin >> r;
+                if((20-r)>0)
+                    r=20-r;
+                else
+                    r=0;
+                tot+=300+r;
+                //cout << tot << endl;
+            }
+            else if(s=="TOP_CONTRIBUTOR"){
+                tot+=300;
+                //cout << tot << endl;
+            }
+            else if(s=="BUG_FOUND"){
+                ll r;
+                cin >> r;
+                tot+=r;
+                //cout << tot << endl;
+            }
+            else if(s=="CONTEST_HOSTED"){
+                tot+=50;
+                //cout << tot << endl;
+            }
+            }
+        cout << tot/n << endl;
+    }
+    return 0;
+}
